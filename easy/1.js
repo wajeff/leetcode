fbeed37ec42005aeaf95d17d98d2d2f35f00ef28
WaeBae <jrwuoft@gmail.com>
2024-04-27 20:29:49 +0800

    refactored 1.js

--- easy/1.js
+++ easy/1.js
@@ -1,17 +1,9 @@
-var twoSum = function(nums, target) {    
-    variableTarget=target
-    variableNums=[...nums]
-    output=[]
-    console.log(nums.indexOf(target))
-    nums.forEach((element,index)=> {
-        variableTarget-=element
-        variableNums[index]=null
-        output.push(variableNums.indexOf(variableTarget))
-        variableNums=[...nums]
-        variableTarget=target
-    });
-
-    output=output.filter((element)=>element>-1)
-    return(output)
+var twoSum = function(nums, target) {   
+    for(let i = 0; i<nums.length ; i++){
+        valueToFind = target - nums[i]
+        if(nums.indexOf(valueToFind,i+1)>-1){
+            return [i,nums.indexOf(valueToFind,i+1)]
+        }
+    }
 };
-twoSum([2,7,11,15],9)
+twoSum([3,2,4],6)
