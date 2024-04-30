3826f51877dde58581355172d25e387b1e5071a0
WaeBae <jrwuoft@gmail.com>
2024-04-30 22:37:09 +0800

    5.js time exceeded but functional

--- easy/5.js
+++ easy/5.js
@@ -1,3 +1,26 @@
-var longestPalindrome = function(s) {
-    
+var longestPalindrome = function(str) {
+    let palindromes = [];
+    let leftPointer = 0;
+    let rightPointer = 0;
+
+    while(leftPointer<str.length){
+        rightPointer=leftPointer
+        let currentStringArray=[]
+        while(rightPointer<str.length){
+            currentStringArray.push(str.charAt(rightPointer))
+            let reverseCurrentStringArray=[...currentStringArray].reverse()
+            if(reverseCurrentStringArray.join('')==currentStringArray.join('')){
+
+                palindromes.push(currentStringArray.join(''))
+            }   
+            rightPointer++
+        }
+        leftPointer++
+
+    }
+    let output=palindromes.sort((a,b)=>b.length-a.length)
+    return output[0]
 };
+longestPalindrome('cbbd') //=>'bab'
+                // l
+                // r
