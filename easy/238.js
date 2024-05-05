d7c0db0ceb4a4f3f7c96d67cc433888ee705c8b9
WaeBae <jrwuoft@gmail.com>
2024-05-06 00:17:02 +0800

    238.js refactored to 10 lines, invalid output with negative elements in input array

--- easy/238.js
+++ easy/238.js
@@ -1,18 +1,12 @@
 var productExceptSelf = function(nums) {
+    let numsString=[...nums].join('')
     let output = []
-
-    let numsMap = new Map()
-    nums.forEach((element,index)=>{
-        numsMap.set(index,element)
-    })
-    for (let i = 0; i<nums.length ; i++){
-        let currentMap = new Map(numsMap.entries())
-        currentMap.delete(i)
-        const sum = Array.from(currentMap.values()).reduce((acc, value) => acc * value, 1);
-        output.push(sum)
+    for( let i = 0; i < nums.length ; i ++){
+        currentString=numsString.replace(numsString[i],'')
+        output.push(currentString.split('').reduce((a,b)=>a*b,1))
+        
     }
     return(output)
-
 };
 productExceptSelf([1,2,3,4])
 //[24,12,8,6]
