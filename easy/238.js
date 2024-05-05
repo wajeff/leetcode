5248362df4bb3df4b24adf5b3a9e383a81d0f9cc
WaeBae <jrwuoft@gmail.com>
2024-05-06 00:44:29 +0800

    238.js completed but runs faster than only 11% of solutions

--- easy/238.js
+++ easy/238.js
@@ -1,12 +1,30 @@
 var productExceptSelf = function(nums) {
-    let numsString=[...nums].join('')
-    let output = []
-    for( let i = 0; i < nums.length ; i ++){
-        currentString=numsString.replace(numsString[i],'')
-        output.push(currentString.split('').reduce((a,b)=>a*b,1))
-        
+    let leftArray=[]
+    let rightArray=[]
+    let output=[]
+
+    for(let i = 0; i<nums.length;i++){
+        if(i==0){
+            leftArray.push(1)
+        }
+        else{
+            leftArray.push(leftArray[i-1]*nums[i-1])
+        }
     }
-    return(output)
+    for(let i = nums.length-1; i>=0 ;i --){
+        if(i==nums.length-1){
+            rightArray[i]=(1)
+        }
+        else{
+            rightArray[i]=rightArray[i+1]*nums[i+1]
+        }
+    }
+    for(let i = 0 ; i<leftArray.length ; i++){
+        output.push(leftArray[i]*rightArray[i])
+    }
+    return output
+    
+
 };
 productExceptSelf([1,2,3,4])
 //[24,12,8,6]
