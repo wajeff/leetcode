f4a45a50d0f9237de4c4f8e633863b899f194174
WaeBae <jrwuoft@gmail.com>
2024-05-09 19:23:23 +0800

    completed 9.js using sliding window beats 57% of users in speed beats 10% of users in speed requires refactoring

---
+++ easy/9.js
@@ -0,0 +1,41 @@
+var isPalindrome = function(num) {
+    num = num.toString()
+    let numLength = num.length-1
+    if(num[0]=='-'){
+        return false
+    }
+    if(num.length%2==0){
+        left = (Math.floor(numLength/2))
+        right = left++
+        while(num[left]){
+            
+            if(num[left]==num[right]){
+                left--
+                right++
+            }
+            else{
+                return(false)
+            }
+        }
+        return true
+    }
+    else if(num.length%2!=0){
+        left =Math.floor(numLength/2);
+        right = Math.floor(numLength/2);
+
+        while(num[left]){
+            if(num[left]==num[right]){
+                left--
+                right++
+            }
+            else{
+                return false
+            }
+        }
+        return true
+
+    }
+
+
+};
+isPalindrome(12321)
