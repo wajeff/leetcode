d6bfc7a789b68f1937b5e3754f567600af3ca080
WaeBae <jrwuoft@gmail.com>
2024-05-13 16:22:37 +0800

    refactored 74.js beats 99% of time complexity beats 70% of memory

--- easy/74.js
+++ easy/74.js
@@ -1,10 +1,8 @@
 var searchMatrix = function(matrix, target) {
-
-    if (matrix.flat().indexOf(target)!=-1){
-        return true
+    for(row in matrix){
+        if (matrix[row].indexOf(target)!=-1){
+            return true
+        }
     }
-    else{
-        return false
-    }
+    return false
 };
-searchMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]],3)
