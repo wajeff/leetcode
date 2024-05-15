d99977318e45744b8bee9c28245a1233dfbc10d0
WaeBae <jrwuoft@gmail.com>
2024-05-15 22:30:13 +0800

    13.js refactored beats 80% of users in speed beats 90% of users in memory

--- easy/13.js
+++ easy/13.js
@@ -1,20 +1,20 @@
 var romanToInt = function(s) {
-    const values = new Map([
-       ['I', 1],
-       ['V', 5],
-       ['X', 10],
-       ['L', 50],
-       ['C', 100],
-       ['D', 500],
-       ['M', 1000],
-    ])
+    const values = {
+       'I': 1,
+       'V': 5,
+       'X': 10,
+       'L': 50,
+       'C': 100,
+       'D': 500,
+       'M': 1000,
+    }
     let output=0
     for ( let i = 0 ; i < s.length ; i++){
-        if (values.get(s[i])<(values.get(s[i+1]))){
-            output-=values.get(s[i])
+        if (values[s[i]]<values[s[i+1]]){
+            output-=values[s[i]]
         }
         else{
-            output+=values.get(s[i])
+            output+=values[s[i]]
         }
     }
     return(output)
