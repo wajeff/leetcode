2b8f81e75813573ff46456b2152100e7372e4c9b
WaeBae <jrwuoft@gmail.com>
2024-08-15 12:04:48 -0400

    Easy
    Completed 121.js
    Moved 121.js from incomplete to main directory

---
+++ easy/121 (incomplete).js
@@ -0,0 +1,16 @@
+var maxProfit = function(prices) {
+  let buy = prices[0]
+  let profit = 0
+
+  for (let i = 0; i<prices.length; i++){
+    if(buy>prices[i]){
+      buy=prices[i];
+    } else{
+      profit = Math.max(prices[i]-buy, profit);
+    }
+  };
+  return profit
+};
+
+maxProfit([5,3,6,4]
+)
