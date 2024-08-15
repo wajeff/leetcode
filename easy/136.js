34a4447851ef4b61f24da4995f9937bbd63104db
WaeBae <jrwuoft@gmail.com>
2024-08-15 13:00:18 -0400

    Easy Completed second solution to 136.js

--- easy/136.js
+++ easy/136.js
@@ -1,4 +1,21 @@
 var singleNumber = function(nums) {
+
+  while(nums.length!==1){
+    for(i=0;i<nums.length;i++){
+      if (nums.indexOf(nums[i]) != nums.lastIndexOf(nums[i])){
+        nums=nums.filter((value)=>value!=nums[i])
+      }
+    }
+  }
+
+  
+  return(nums)
+
+};
+singleNumber([1,1,2,3,4,5,4,5,3,-1,2])
+
+/*First Solution 
+var singleNumber = function(nums) {
   result=[]
   for(i=0;i<nums.length;i++){
     
@@ -8,5 +25,4 @@ var singleNumber = function(nums) {
     }
   }
   return(result[0])
-};
-singleNumber([4,1,2,1,2])
+};*/
