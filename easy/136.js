4db1d68a558faf9608ee84886f4b8c7cbe5133d7
WaeBae <jrwuoft@gmail.com>
2024-08-15 20:35:42 -0400

    Easy Completed 136.js

--- easy/136.js
+++ easy/136.js
@@ -1,6 +1,24 @@
 var singleNumber = function(nums) {
+  const hash = {}
+  for (const num of nums){
+    if(!hash[num]){
+      hash[num]=1
+    }
+    else{
+      hash[num]++
+    }
+  }
+  for(const key in hash){
+    if(hash[key]==1){
+      return(key)
+    }
+  }
 
-  while(nums.length!==1){
+};
+
+singleNumber([1,1,2,3,4,5,4,5,3,-1,2])
+/*Second Solution 
+while(nums.length!==1){
     for(i=0;i<nums.length;i++){
       if (nums.indexOf(nums[i]) != nums.lastIndexOf(nums[i])){
         nums=nums.filter((value)=>value!=nums[i])
@@ -11,8 +29,9 @@ var singleNumber = function(nums) {
   
   return(nums)
 
-};
-singleNumber([1,1,2,3,4,5,4,5,3,-1,2])
+*/
+  
+
 
 /*First Solution 
 var singleNumber = function(nums) {
