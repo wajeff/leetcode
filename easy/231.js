3d4cdda4f2947e1b16649b5ea53be1599f8b8deb
WaeBae <jrwuoft@gmail.com>
2024-08-20 01:21:45 -0400

    Easy Significantly improved 231.js

--- easy/231.js
+++ easy/231.js
@@ -1,5 +1,13 @@
 var isPowerOfTwo = function(n) {
-  let powersOfTwo = 0
+  if(Math.log2(n)%1==0){
+    return true
+  }
+  return false
+  
+};
+
+/*  First Solution smh
+let powersOfTwo = 0
   let i = 0
   while(powersOfTwo<=0){
     let powersOfTwo = Math.pow(2,i)
@@ -13,22 +21,6 @@ var isPowerOfTwo = function(n) {
       }
     i++
   }
+ */
 
-  
-};
-
-/*  if(n==1||n==2){
-      return true
-  }
-  if(n%2!=0 ){
-      return false
-  }
-  if(n>=8 && n%8==0 && n!=24){
-      return true
-  }    
-  else if(n<8 && n%2==0 && n%4==0){
-      return true
-  }
-  return false
-   */
 isPowerOfTwo(32)
