565abddaf1fc32162e0cc66554086715bc0ba80c
WaeBae <jrwuoft@gmail.com>
2024-08-23 17:52:04 -0400

    Easy Changed algo for 202.js using while loop instead of recursion

--- easy/202.js
+++ easy/202.js
@@ -1,5 +1,23 @@
 var isHappy = function(n) {
 
+  while (String(n).length!=1){
+    n = Array.from(String(n).split(''),Number);
+    n = n.map((element)=>element**2);
+    n = n.reduce((a,b)=>a+b,0);
+
+  }
+  if(n==1 || n==7){
+    return true
+  }
+  else{
+
+    return false
+  }
+};
+isHappy(1111111)
+
+/*var isHappy = function(n) {
+
   recursion = function(n){
     if (n == 1 || n== 7){
 
@@ -19,5 +37,4 @@ var isHappy = function(n) {
   }
   return(recursion(n))
   
-};
-isHappy(1111111)
+}; */
