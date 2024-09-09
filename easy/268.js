fd22be9194d603357c8e0ac647f541432edc1ea6
WaeBae <jrwuoft@gmail.com>
2024-09-09 10:28:44 -0400

    Easy
    Refactored 268.js using Gauss formula and subtracting the value of the array.reduce using addition

--- easy/268.js
+++ easy/268.js
@@ -1,21 +1,32 @@
 var missingNumber = function(nums) {
-  const sortedNums = [...nums].sort((a,b)=>a-b)
-  console.log(sortedNums)
-  for (let i = 0; i < nums.length; i++){
-    if(i==0){
-      if(sortedNums[i]!==0){
-        console.log('break')
-        return 0
-      }
-    }
-    else if(sortedNums[i]!==sortedNums[i-1]+1){
-      console.log(true)
-      return sortedNums[i-1]+1
-    }
+  function sumTo(n) {
+    return n * (n + 1) / 2;
   }
-  if(!sortedNums.includes(sortedNums.length)){
-    return sortedNums.length
+  console.log(sumTo(nums.length))
+  const numsValue = [...nums].reduce((a,b)=>a+b,0)
+  if(numsValue!=sumTo(nums.length)){
+    return sumTo(nums.length)-numsValue
+  }
+  else{
+    return 0
   }
 };
 
-missingNumber([ 0, 1 ])
+missingNumber([3,0,1])
+/*const sortedNums = [...nums].sort((a,b)=>a-b)
+console.log(sortedNums)
+for (let i = 0; i < nums.length; i++){
+  if(i==0){
+    if(sortedNums[i]!==0){
+      console.log('break')
+      return 0
+    }
+  }
+  else if(sortedNums[i]!==sortedNums[i-1]+1){
+    console.log(true)
+    return sortedNums[i-1]+1
+  }
+}
+if(!sortedNums.includes(sortedNums.length)){
+  return sortedNums.length
+}*/
