b651fb3f2231abcaad87ce722d8f17985e31d994
WaeBae <jrwuoft@gmail.com>
2024-09-09 15:18:59 -0400

    Easy
    263.js refactored again, still using isPrime, time limit exceeded

--- easy/263.js
+++ easy/263.js
@@ -1,25 +1,20 @@
 var isUgly = function(n) {
-  if(n<1){
-    return 0
-  }
   function isPrime(num) {
     for (let i = 2; i <= Math.sqrt(num); i++) {
       if (num % i === 0) return false; // If the number is divisible by any number other than 1 and itself, it's not a prime
     }
     return true; // Return true if the number is prime
   }
-  const primes = []
-  for(let i = 2 ; i < n+1 ; i++){
-    if(isPrime(i)&&n%i===0){
-      primes.push(i)
-    }
-  }
-
-  for(number of primes){
-    if(![2,3,5].includes(number)){
-      return false
+  for (let i = 2; i <= n; i++) {
+    // While 'i' is a prime factor and divides 'num' evenly
+    while (isPrime(i) && n % i === 0) {
+  
+      if(![2,3,5].includes(i)){
+        return false
+      }
+      n /= i; 
     }
   }
   return true
 };
-isUgly(7)
+isUgly(937351770)
