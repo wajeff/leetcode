f160d6428f51021f6bc6e2905a9d52d1756ba766
WaeBae <jrwuoft@gmail.com>
2024-09-11 14:19:35 -0400

    Easy 504.js completed 520.js removed console logs

---
+++ easy/504.js
@@ -0,0 +1,4 @@
+var convertToBase7 = function(num) {
+  return(num.toString(7))
+};
+convertToBase7(14)
--- easy/520.js
+++ easy/520.js
@@ -1,22 +1,15 @@
 var detectCapitalUse = function(word) {
   const regex = /[A-Z]/g
   const wordWithoutFirstLetter = word.substring(1)
-
   if(word === word.toUpperCase()){
-    console.log(true)
     return true
   }
   if(word === word.toLowerCase()){
-
     return true
   }
-
-
   if(regex.test(word.charAt(0))&&(wordWithoutFirstLetter.search(regex)==-1)){
-
     return true
   }
   return false
-
 };
 detectCapitalUse("Niy")
