dee221765764466c92f4cb0c8fe97005a6594a63
WaeBae <jrwuoft@gmail.com>
2024-09-11 15:21:15 -0400

    Easy
    500.js completed

---
+++ easy/500.js
@@ -0,0 +1,17 @@
+var findWords = function(words) {
+  const topRow = new RegExp (/^[qwertyuiop]+$/gi)
+  const middleRow = new RegExp (/^[asdfghjkl]+$/gi)
+  const bottomRow = new RegExp (/^[zxcvbnm]+$/gi)
+
+  let result = []
+  for (word of words){
+
+
+    if(word.search(topRow)!==-1||word.search(middleRow)!==-1 || word.search(bottomRow)!==-1 ){
+      result.push(word)
+    }
+
+  }
+  return(result)
+};
+findWords(["asdfghjkla","qwertyuiopq","zxcvbnzzm","asdfghjkla","qwertyuiopq","zxcvbnzzm"])
