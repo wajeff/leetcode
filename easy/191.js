30a5c04f30fad05fd61f485aac9ad7eaeb12fb18
WaeBae <jrwuoft@gmail.com>
2024-09-18 14:18:23 -0400

    Easy 191.js completed

---
+++ easy/191.js
@@ -0,0 +1,11 @@
+var hammingWeight = function(n) {
+  let setBitCount = 0
+  for (number of n.toString(2).split('')){
+    if(number==='1'){
+      setBitCount++
+    }
+  }
+
+ return setBitCount 
+};
+hammingWeight(128)
