263cce59ee8f6b3fa4f49a48860069715201e319
WaeBae <jrwuoft@gmail.com>
2024-09-23 20:37:10 -0400

    Easy 349.js refactored

--- easy/349.js
+++ easy/349.js
@@ -1,11 +1,14 @@
 var intersection = function(nums1, nums2) {
-  let result = []
+  let filteredArrays = nums1.filter((element)=>nums2.includes(element))
+  set = new Set(filteredArrays)
+  return Array.from(set)
+};
+/*  
+let result = []
   for(num of nums1){
     if(nums2.includes(num) && !result.includes(num)){
       result.push(num)
     }
   }
-  return(result)
-
-};
+  return(result) */
 intersection([1,2,2,1],[2,2])
