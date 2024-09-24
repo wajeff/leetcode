afcb658dec45e24e74e94cd3184f2519792a398b
WaeBae <jrwuoft@gmail.com>
2024-09-23 20:22:04 -0400

    Easy 169.js completed

---
+++ easy/169.js
@@ -0,0 +1,18 @@
+var majorityElement = function(nums) {
+  let numsCount = new Map()
+  for(num of nums){
+    if(!numsCount.get(num)){
+      numsCount.set(num, 1)
+    }
+    else{
+      numsCount.set(num,numsCount.get(num)+1)
+    }
+  }
+  for([key,val] of numsCount){
+
+    if(val>=nums.length/2){
+      return(key)
+    }
+  }
+};
+majorityElement([3,2,3])
--- easy/283.js
+++ easy/283.js
@@ -1,12 +1,21 @@
 var moveZeroes = function(nums) {
-  for(num of nums){
+  for(let i = 0; i < nums.length; i++){
+    if(nums[i] === 0){
+      nums[nums.length - 1] = nums.splice(nums[i],1)
+    }
+  }
+  console.log(nums.flat())
+  return(nums.flat())
+};
+
+moveZeroes([0,1,0,3,12])
+
+/* 
+for(num of nums){
     if(num===0){
-      // zeroCount++
       nums.splice(nums.indexOf(0),1)
       nums.push(0)
     }
   }
 
-  return nums
-}
-moveZeroes([0,0,1])
+return nums */
