9aa19acdaa154c04c9faf74530de9ec1a363994d
WaeBae <jrwuoft@gmail.com>
2024-09-26 10:38:06 -0400

    Easy 414.js completed

---
+++ easy/414.js
@@ -0,0 +1,21 @@
+var thirdMax = function(nums) {
+  if(nums.length==1){
+    return nums[0]
+  }
+
+  nums = nums.sort((a,b)=>b-a)
+  nums = new Set(nums)
+  nums = Array.from(nums)
+
+  if(nums.length==1){
+    return nums[0]
+  }
+  if(nums.length==2){
+
+
+    return nums[0]
+  }
+
+  return nums[2]
+};
+thirdMax([1,1,1])
