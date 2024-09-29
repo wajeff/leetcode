17f148b9a3ba8c58f30192759f56fb38d64e2f5c
WaeBae <jrwuoft@gmail.com>
2024-09-29 16:37:07 -0400

    Easy 455.js completed

--- easy/455.js
+++ easy/455.js
@@ -1,22 +1,35 @@
-var findContentChildren = function(children, cookie) {
-  cookie=cookie.sort((a,b)=>b-a)
-  children=children.sort((a,b)=>b-a)
-  cookie.filter((element)=>!element<children[children.length-1])
+var findContentChildren = function(children, cookies) {
 
-  cookieCount=0
-  for(let i = 0; i < children.length ; i++){
-    for(let j = 0; j < cookie.length ; j++){
-      if(cookie[j]<children[i]){
-        continue;
-      }
-      else if(cookie[j]>=children[i]){
-        cookieCount++
-        cookie.splice(cookie.indexOf(cookie[j]),1)
-        break;
-      }
+  children = children.sort((a,b)=>a-b)
+  cookies = cookies.sort((a,b)=>a-b)
+
+  let topPointer = 0
+  let bottomPointer = 0
+  let cookieCounter = 0
+
+  while (bottomPointer<cookies.length){
+    if(children[topPointer]<=cookies[bottomPointer]){
+      cookieCounter++
+      topPointer++
+      bottomPointer++
+    }
+    else{
+      bottomPointer++
     }
   }
 
-  return(cookieCount)
+  return(cookieCounter)
 };
-findContentChildren([1,2,3],[1,1])
+findContentChildren(
+  [250,490,328,149,495,325,314,360,333,418,430,458],
+[
+   1,  1,  1,  2,  2,  2,  2,  2,  2,  2,  2,  2,
+   2,  2,  3,  3,  3,  3,  3,  4,  4,  4,  4,  4,
+   4,  4,  4,  4,  4,  4,  5,  5,  5,  5,  5,  5,
+   5,  5,  5,  6,  6,  6,  6,  7,  7,  8,  8,  8,
+   8,  8,  9,  9,  9,  9,  9,  9,  9, 10, 10, 10,
+  10, 11, 11, 11, 11, 11, 11, 12, 12, 12, 12, 12,
+  13, 13, 13, 13, 13, 13, 13, 13, 14, 14, 14, 14,
+  14, 14, 14, 14, 14, 14, 15, 15, 15, 15, 15, 15,
+  16, 16, 16, 16]
+)
