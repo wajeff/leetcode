6f8b6f3410d823e842a2ae603be555430450864b
WaeBae <jrwuoft@gmail.com>
2024-09-29 18:00:08 -0400

    Easy 506.js completed

---
+++ easy/506.js
@@ -0,0 +1,22 @@
+var findRelativeRanks = function(score) {
+  sortedScore = [...score].sort((a,b)=>b-a)
+  console.log(sortedScore)
+  console.log(score)
+  for(index in sortedScore){
+    console.log(typeof(index))
+    if(index == 0){
+      score.splice(score.indexOf(sortedScore[index]),1,'Gold Medal')
+    }
+    else if(index == 1){
+      score.splice(score.indexOf(sortedScore[index]),1,'Silver Medal')
+    }
+    else if(index == 2){
+      score.splice(score.indexOf(sortedScore[index]),1,'Bronze Medal')
+    }
+    else{
+      score.splice(score.indexOf(sortedScore[index]),1,String(Number(index)+1))
+    }
+  }
+  return(score)
+};
+findRelativeRanks([10,3,8,9,4])
