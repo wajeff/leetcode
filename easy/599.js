3e8b7fc0aa629190a7deab7d2c8248f7fa18e5b8
WaeBae <jrwuoft@gmail.com>
2024-09-30 16:37:41 -0400

    Easy 599.js refactored, 20% faster, similar memory use

--- easy/599.js
+++ easy/599.js
@@ -1,5 +1,26 @@
 var findRestaurant = function(list1, list2) {
-  commonList = list1.filter((element)=>list2.includes(element))
+  indexes=[]
+  commonList=[]
+  map = new Map()
+  for(word of list1){
+    if(list2.includes(word)){
+      map.set(word,list1.indexOf(word)+list2.indexOf(word))
+    }
+  }
+  const mapSort = new Map([...map.entries()].sort((a, b) => a[1] - b[1]))
+  
+  minimumIndex = mapSort.entries().next().value[1]
+  result = []
+  for( pair of mapSort){
+    if(pair[1]==minimumIndex){
+      result.push(pair[0])
+    }
+  }
+  return(result)
+};
+findRestaurant(["Shogun","Piatti","Tapioca Express","Burger King","KFC"],["Piatti","The Grill at Torrey Pines","Hungry Hunter Steakhouse","Shogun"])
+
+/* commonList = list1.filter((element)=>list2.includes(element))
   indexes=[]
   for(word of commonList){
     indexes.push(list1.indexOf(word)+list2.indexOf(word))
@@ -13,6 +34,4 @@ var findRestaurant = function(list1, list2) {
     }
   }
   console.log(result)
-  return result
-};
-findRestaurant(["Shogun","Piatti","Tapioca Express","Burger King","KFC"],["Piatti","The Grill at Torrey Pines","Hungry Hunter Steakhouse","Shogun"])
+  return result */
