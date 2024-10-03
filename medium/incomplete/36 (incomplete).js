86b0edb190c2e8b08a766d348b8045bbe67acaca
WaeBae <jrwuoft@gmail.com>
2024-10-03 16:03:31 -0400

    Medium 36.js completed beats 92%

--- medium/incomplete/36 (incomplete).js
+++ medium/incomplete/36 (incomplete).js
@@ -3,22 +3,50 @@
  * @return {boolean}
  */
 var isValidSudoku = function(board) {
-    let indexOfValues = []
-    let columnValues = []
-    let topRow = board[0]
-
-    for(let i = 0; i <topRow.length ; i ++){
-        
-        for (let j = 0; j <topRow.length ;j++){
-            if(topRow[i].includes(board[i][j])&&topRow[i]!='.'){
-                return false
-            }
-        }  
-        columnValues = []
+  
+  numsRegex = new RegExp (/^[0-9]*$/)
+  
+  for(array of board){
+    tempArray=[...array].filter((element)=>element!='.') //filter out empty
+    arrayWithoutDotsLength = tempArray.length
+    set = new Set(tempArray)
+    if(set.size!=arrayWithoutDotsLength){
+      return false
     }
-    return true
-};
+  }
+  flatBoard = [...board].flat()
 
+  for(index in flatBoard){
+    incrementIndex = Number(index)
+    while(incrementIndex<flatBoard.length){
+      incrementIndex+=9
+      if(flatBoard[index]==flatBoard[incrementIndex]&&flatBoard[index]!='.'){
+        return false
+      }
+    }
+  }
+  let res = [];
+  for (let i = 0; i < 9; i += 3) {
+    for (let j = 0; j < 9; j += 3) {
+      let curr = [];
+      for (let k = 0; k < 3; k++) {
+        for (let l = 0; l < 3; l++) {
+          curr.push(board[i + k][j + l]);
+      }
+    }
+    res.push(curr);
+  }
+  for(array of res){
+    array=array.filter((element)=>element!='.')
+    let set = new Set(array)
+    if(set.size!=array.length){
+      return false
+    }
+  }
+
+}
+  return true
+};
 
 isValidSudoku(
 [["5","3",".",".","7",".",".",".","."]
@@ -30,3 +58,23 @@ isValidSudoku(
 ,[".","6",".",".",".",".","2","8","."]
 ,[".",".",".","4","1","9",".",".","5"]
 ,[".",".",".",".","8",".",".","7","9"]])
+// isValidSudoku(
+//   [["8","3",".",".","7",".",".",".","."]
+//   ,["6",".",".","1","9","5",".",".","."]
+//   ,[".","9","8",".",".",".",".","6","."]
+//   ,["8",".",".",".","6",".",".",".","3"]
+//   ,["4",".",".","8",".","3",".",".","1"]
+//   ,["7",".",".",".","2",".",".",".","6"]
+//   ,[".","6",".",".",".",".","2","8","."]
+//   ,[".",".",".","4","1","9",".",".","5"]
+//   ,[".",".",".",".","8",".",".","7","9"]])
+// isValidSudoku(
+//   [[".",".",".",".","5",".",".","1","."]
+//   ,[".","4",".","3",".",".",".",".","."]
+//   ,[".",".",".",".",".","3",".",".","1"]
+//   ,["8",".",".",".",".",".",".","2","."]
+//   ,[".",".","2",".","7",".",".",".","."]
+//   ,[".","1","5",".",".",".",".",".","."]
+//   ,[".",".",".",".",".","2",".",".","."]
+//   ,[".","2",".","9",".",".",".",".","."]
+//   ,[".",".","4",".",".",".",".",".","."]])
