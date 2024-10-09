c94c70b233bb1ce62f34e7bec14f737fc66eb146
WaeBae <jrwuoft@gmail.com>
2024-10-09 15:48:25 -0400

    Medium 53.js completed

--- medium/53.js
+++ medium/53.js
@@ -1,28 +1,21 @@
 var maxSubArray = function(nums) {
-  let arrayReduced = [...nums].reduce((a,b)=>a+b,0)
-  let max = arrayReduced
-  let left = 0
-  let right = nums.length-1
-
-  while (left < right){
-    if(arrayReduced-nums[left]>arrayReduced){
-      arrayReduced-=nums[left]
-      left++
-    }
-    else if(arrayReduced-nums[right]>arrayReduced){
-      arrayReduced-=nums[right]
-      right--
-    }
-    else{
-      if(arrayReduced>max){
-        max=arrayReduced
-      }
-      left++
-      
-    }
+  if(nums.length==1){
+    return nums[0]
   }
-  console.log(arrayReduced)
+  let accumulatedNumber = nums[0]
+  let max = nums[0]
+  for(let i = 1; i < nums.length ; i++){
+    if(accumulatedNumber+nums[i]>nums[i]){
+      accumulatedNumber+=nums[i]
+    }
+    else if(accumulatedNumber+nums[i]<=nums[i]){
+      accumulatedNumber=nums[i]
+    }
+
+    max = Math.max(accumulatedNumber,max)
+  }
   console.log(max)
+  return(max)
 };
 
-maxSubArray([5,4,-1,7,8])
+maxSubArray([1,2,-1,-2,2,1,-2,1,4,-5,4])
