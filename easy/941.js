709bc50ba573cca0b03474e2f0049dcf3cc683cb
WaeBae <jrwuoft@gmail.com>
2024-11-02 15:07:13 -0400

    Easy 941.js added commas

--- easy/941.js
+++ easy/941.js
@@ -1,5 +1,5 @@
 var validMountainArray = function(arr) {
-  let hill = 0
+  let hill = 0;
   for(let i = 0; i < arr.length ; i++){
     if(arr[i]>arr[i+1]){
       hill = i
@@ -7,13 +7,13 @@ var validMountainArray = function(arr) {
     }
     else if(arr[i]===arr[i+1]){
       return false
-    }
-  }
+    };
+  };
   if(hill==0 || hill ==arr.length-1){
     return false
-  } 
-  let left = hill
-  let right = hill
+  };
+  let left = hill;
+  let right = hill;
   while(left>0||right<arr.length){
     if(!(arr[left]>arr[left-1])&&arr[left-1]){
       return false
@@ -24,7 +24,7 @@ var validMountainArray = function(arr) {
     left--
     right++
   };
-  return true
+  return true;
   
 };
 validMountainArray([1,7,9,5,4,1,2])
