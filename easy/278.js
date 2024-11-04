1a7d71d06d6bc24c6cab8a83b40d9b9f62c7d3cd
WaeBae <jrwuoft@gmail.com>
2024-11-04 11:36:52 -0500

    Easy 278.js added commas refactored to remove unnecessary lines

--- easy/278.js
+++ easy/278.js
@@ -1,17 +1,16 @@
 var solution = function(isBadVersion) {
   return function(n) {
-    let start = 0 
-    let end = n
-    let middle = 0
+    let start = 0; 
+    let end = n;
     while(start<=end){
-      middle = Math.floor((end+start)/2)
+      middle = Math.floor((end+start)/2);
       if(isBadVersion(middle)){
-        end = middle-1
+        end = middle-1;
       }
       else{
-        start = middle+1
-      }
-    }
-    return start
+        start = middle+1;
+      };
+    };
+    return start;
   };
 };
