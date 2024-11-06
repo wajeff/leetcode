2e861a7a762e2142d14c1f13649d97d892999339
WaeBae <jrwuoft@gmail.com>
2024-11-06 18:19:54 -0500

    Easy 704.js completed

---
+++ easy/704.js
@@ -0,0 +1,23 @@
+var search = function(nums, target) { 
+  let start = 0
+  let end = nums.length-1
+  if(nums.length===1){
+    if(nums[0]===target){
+      return 0
+    }
+  }
+  while(start<=end){
+    let middle = Math.floor((start+end)/2)
+    if(nums[middle]===target){
+      return middle
+    }
+    else if(nums[middle]<target){
+      start = middle+1
+    }
+    else if(nums[middle]>target){
+      end = middle-1
+    }
+  }
+  return -1
+};
+search([2,5],5)
