ea0e71fed25e5bcd31a5a116f2ae1b25ae9bb539
WaeBae <jrwuoft@gmail.com>
2024-11-06 19:20:57 -0500

    Easy 441.js refactored removed redundant variables added commas

--- easy/441.js
+++ easy/441.js
@@ -1,13 +1,12 @@
 var arrangeCoins = function(n) {
   if(n===1){
     return 1
-  }
+  };
   for(let i = 0; i<n ;i++){
-    let gaussNumber = (i*(i+1)/2)
-    let nextGaussNumber = ((i+1)*(i+2)/2)
+    let nextGaussNumber = ((i+1)*(i+2)/2);
     if(nextGaussNumber>n){
       return i
-    }
+    };
   };
 };
 arrangeCoins(3)
