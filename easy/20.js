c9c950654a430f47bc6fd5c2dc6b697af60c01f7
WaeBae <jrwuoft@gmail.com>
2024-11-12 21:19:14 -0500

    Easy 20.js refactored to be more readable

--- easy/20.js
+++ easy/20.js
@@ -4,14 +4,16 @@
  */
 var isValid = function(s) {
   let stack = [];
-  for( let i = 0 ; i < s.length ; i++){
-    if(s[i]==='['||s[i]==='('||s[i]==='{'){
-      stack.push(s[i]);
+  for( charc of s){
+    if(charc==='['||charc==='('||charc==='{'){
+      stack.push(charc);
     }
     else{
       if(stack.length===0) return false;
       let pop = stack.pop();
-      if(!(pop==='('&&s[i]===')')&&!(pop==='['&&s[i]===']')&&!(pop==='{'&&s[i]==='}')){
+      if(!(pop==='('&&charc===')') &&
+        (!(pop==='['&&charc===']')) &&
+        (!(pop==='{'&&charc==='}'))){
         return false;
       };
     };
