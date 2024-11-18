c72279abce759f842b7b40a4e109177747878f63
WaeBae <jrwuoft@gmail.com>
2024-11-17 21:25:39 -0500

    Easy 338.js refactored to be more readable

--- easy/338.js
+++ easy/338.js
@@ -1,9 +1,9 @@
 var countBits = function(n) {
   let ans = [];
   for(let i = 0; i <= n; i++){
-    ans.push(i.toString(2).split('').filter((element)=>element!=='0').join('').length);
+    ans.push(i.toString(2).replaceAll('0','').length);
   };
   return(ans);
   
 };
-countBits(2);
+countBits(5);
