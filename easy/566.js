f45b559d3861bb33750dad6955d2c5e70bf0ad64
WaeBae <jrwuoft@gmail.com>
2024-11-25 18:40:03 -0500

    Easy 566.js completed

--- easy/566.js
+++ easy/566.js
@@ -1,18 +1,13 @@
 var matrixReshape = function(mat, r, c) {
   let matFlat = [...mat].flat();
   let result = [];
-  let currentArray = [];
   if(r * c == matFlat.length){
-    for (values of matFlat){
-      currentArray.push(values)
-      if(currentArray.length===c){
-        result.push(currentArray)
-        currentArray=[]
-      }l
+    while (matFlat.length){
+      result.push(matFlat.splice(0,c))
     };
     return (result)
   };
   
   return mat;
 };
-matrixReshape([[1,2],[3,4]],2,2)
+matrixReshape([[1,2],[3,4]],1,4)
