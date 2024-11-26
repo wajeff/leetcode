e7a760fd4046c32c2b0fa9355efef1080e0f4989
WaeBae <jrwuoft@gmail.com>
2024-11-26 13:29:30 -0500

    Easy 1539.js completed

---
+++ easy/1539.js
@@ -0,0 +1,13 @@
+var findKthPositive = function(arr, k) {
+  let result = [];
+  let number = 1;
+  while(result.length<k){
+    if(!arr.includes(number)){
+      result.push(number)
+    };
+    number++
+  }
+  return(result[k-1])
+
+};
+findKthPositive([2,3,4,7,11],5);
