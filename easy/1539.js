38a051a003561838366c42a6797f889c230399e2
WaeBae <jrwuoft@gmail.com>
2024-11-26 13:30:42 -0500

    Easy 1539.js fixed commas

--- easy/1539.js
+++ easy/1539.js
@@ -5,9 +5,9 @@ var findKthPositive = function(arr, k) {
     if(!arr.includes(number)){
       result.push(number)
     };
-    number++
-  }
-  return(result[k-1])
+    number++;
+  };
+  return(result[k-1]);
 
 };
 findKthPositive([2,3,4,7,11],5);
