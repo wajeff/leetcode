3ab4ff9b71e402301847b9d960db1a9a4338b722
WaeBae <jrwuoft@gmail.com>
2024-11-27 18:09:35 -0500

    Easy 541.js completed

---
+++ easy/541.js
@@ -0,0 +1,17 @@
+var reverseStr = function(s, k) {
+ //explanation
+ //abcdefg => ab swapped
+ //bacdefg => ef swapped
+ //bacdfeg => last index "swapped", but no change
+  const doubleK = 2*k;
+  let result = ''
+  while(s.length>=k){
+    result += s.substring(0,k).split('').reverse().join('');
+    result += s.substring(k,doubleK);
+    s=s.substring(doubleK,s.length);
+  }; 
+  result += s.split('').reverse().join('')
+
+  return(result)
+};
+reverseStr('abcdefg',2) // => 'bacdfeg'
