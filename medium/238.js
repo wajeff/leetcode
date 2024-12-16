f94c2d11d6d484a315e502364cf16420185b4c14
WaeBae <jrwuoft@gmail.com>
2024-12-16 02:53:56 -0500

    Medium 238.js refactored

--- medium/238.js
+++ medium/238.js
@@ -1,30 +1,18 @@
-var productExceptSelf = function(nums) {
-    let leftArray=[]
-    let rightArray=[]
-    let output=[]
+const productExceptSelf = function(nums) {
 
-    for(let i = 0; i<nums.length;i++){
-        if(i==0){
-            leftArray.push(1)
-        }
-        else{
-            leftArray.push(leftArray[i-1]*nums[i-1])
-        }
-    }
-    for(let i = nums.length-1; i>=0 ;i --){
-        if(i==nums.length-1){
-            rightArray[i]=(1)
-        }
-        else{
-            rightArray[i]=rightArray[i+1]*nums[i+1]
-        }
-    }
-    for(let i = 0 ; i<leftArray.length ; i++){
-        output.push(leftArray[i]*rightArray[i])
-    }
-    return output
+  let currentValue = 1;
+  let result = [];
+  for(let i = 0 ; i < nums.length ; i++){
+    result.push(currentValue);
+    currentValue *= nums[i]
     
+  }
+  currentValue = 1;
+  for(let i = nums.length-1; i >-1 ; i--){
+    result[i]=(currentValue*result[i]);
+    currentValue *= nums[i]; 
+    
+  };
+  return(result);
 
-};
-productExceptSelf([1,2,3,4])
-//[24,12,8,6]
+}
